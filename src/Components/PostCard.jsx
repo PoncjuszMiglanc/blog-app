dfd3640c5cbd2699fed5d77a7a24431066e864f2
Miglanc <poncjuszmiglanc@gmail.com>
2023-12-11 14:39:32 +0100

    17th, finished PostCard component: fonts, displaying image, paragraph and class naming convention

--- src/Components/PostCard.jsx
+++ src/Components/PostCard.jsx
@@ -1,76 +1,70 @@
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import '../assets/scss/components/post-card.scss';
-// import pic from '../assets/pic1.jpg';
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import "../assets/scss/components/post-card.scss";
 
 const PostCard = ({ post, style, imgsize }) => {
-	return (
-		<article className="post-card" style={style}>
-			<Link to={`/posts/${post._id}`}>
-				<div className="post-card__wrap">
-					<div className="post-card__content">
-						<img
-							className="info__image"
-							src={`http://localhost:8080/images/${post.image}`}
-							alt="screenshot"
-							style={{ height: `${imgsize}px` }}
-						/>
-						<div className="info__head">
-							<div className="info__meta">
-								<p className="info__author">
-									{/* <a href="">{!post ? '' : post.author}</a> */}
-									{!post ? '' : post.author}
-								</p>
-								<span className="info__separator"></span>
-								{/* <span className="info__date"> */}
-								<time
-									className="info__date"
-									dateTime={
-										!post
-											? ''
-											: new Date(post.createdAt).toLocaleDateString('en-GB')
-									}
-								>
-									{!post
-										? ''
-										: new Date(post.createdAt).toLocaleDateString('pl-PL', {
-												month: 'long',
-												year: 'numeric',
-												day: 'numeric',
-										  })}
-								</time>
-								{/* </span> */}
-							</div>
-							<h2 className="info__title">{!post ? '' : post.title}</h2>
-							<p className="info__sub">
-								{!post ? '' : `${post.lead.substring(1, 100)}...`}
-							</p>
-						</div>
-					</div>
-					<div className="info__tags">
-						<span className="info__tag">
-							<a href="">Film</a>
-						</span>
-						<span className="info__tag">
-							<a href="">Złota palma</a>
-						</span>
-						<span className="info__tag">
-							<a href="">Dramat</a>
-						</span>
-						<span className="info__tag">
-							<a href="">Antonioni</a>
-						</span>
-					</div>
-				</div>
-			</Link>
-		</article>
-	);
+  return (
+    <article className="post-card" style={style}>
+      <Link to={`/posts/${post._id}`}>
+        <div className="post-card__wrapper">
+          <div className="post-card__main-content">
+            <img
+              className="post-card__image"
+              src={`http://localhost:8080/images/${post.image}`}
+              alt="screenshot"
+              style={{ height: `${imgsize}px` }}
+            />
+            <div className="post-card__text">
+              <div className="post-card__meta">
+                <p className="post-card__author">{!post ? "" : post.author}</p>
+                <span className="post-card__separator"></span>
+                <time
+                  className="post-card__date"
+                  dateTime={
+                    !post
+                      ? ""
+                      : new Date(post.createdAt).toLocaleDateString("en-GB")
+                  }
+                >
+                  {!post
+                    ? ""
+                    : new Date(post.createdAt).toLocaleDateString("pl-PL", {
+                        month: "long",
+                        year: "numeric",
+                        day: "numeric",
+                      })}
+                </time>
+              </div>
+              <h2 className="post-card__title">{!post ? "" : post.title}</h2>
+              <p className="post-card__subtitle">
+                {!post ? "" : `${post.lead.substring(1, 200)}...`}
+              </p>
+            </div>
+          </div>
+          <div className="post-card__tags">
+            <span className="post-card__tag">
+              <a href="">Film</a>
+            </span>
+            <span className="post-card__tag">
+              <a href="">Złota palma</a>
+            </span>
+            <span className="post-card__tag">
+              <a href="">Dramat</a>
+            </span>
+            <span className="post-card__tag">
+              <a href="">Antonioni</a>
+            </span>
+          </div>
+        </div>
+      </Link>
+    </article>
+  );
 };
 
 PostCard.propTypes = {
-	post: PropTypes.object,
-	style: PropTypes.object,
-	imgsize: PropTypes.number,
+  post: PropTypes.object,
+  style: PropTypes.object,
+  imgsize: PropTypes.number,
 };
 
 export default PostCard;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,70 +1,72 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 // import { Link } from 'react-router-dom';
-import MainContainer from '../Components/MainContainer';
-import PostCard from '../Components/PostCard';
+import MainContainer from "../Components/MainContainer";
+import PostCard from "../Components/PostCard";
 // import SecondaryPost from '../Components/SecondaryPost';
 // import PostInfo from '../Components/PostInfo';
-import '../assets/scss/pages/home-page.scss';
+import "../assets/scss/pages/home-page.scss";
 
 const HomePage = () => {
-	const [postList, setPostList] = useState([]);
+  const [postList, setPostList] = useState([]);
 
-	const getPostList = () => {
-		fetch('http://localhost:8080/posts')
-			.then((res) => {
-				return res.json();
-			})
-			.then((res) => {
-				setPostList(res.posty.reverse());
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	};
+  const getPostList = () => {
+    fetch("http://localhost:8080/posts")
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        setPostList(res.posty.reverse());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-	useEffect(() => {
-		getPostList();
-	}, []);
+  useEffect(() => {
+    getPostList();
+  }, []);
 
-	return (
-		<MainContainer>
-			<section className="grid">
-				<div className="grid__heading">
-					<h1 className="grid__title" onClick={console.log(postList)}>
-						Filmowy blog
-					</h1>
-					<h2 className="grid__subtitle">Najnowsze teksty</h2>
-				</div>
-				<aside className="grid__sidebar">
-					<ul>
-						<li>1</li>
-						<li>2</li>
-						<li>3</li>
-						<li>4</li>
-						<li>5</li>
-						<li>6</li>
-						<li>7</li>
-						<li>8</li>
-					</ul>
-				</aside>
-				{postList == null
-					? ''
-					: postList.map((post, index) => {
-							if (index == 0) {
-								return (
-									<PostCard
-										key={index}
-										post={post}
-										style={{ gridColumn: 'span 2', gridRow: '2 / span 2' }}
-										imgsize={400}
-									/>
-								);
-							} else {
-								return <PostCard key={index} post={post} />;
-							}
-					  })}
-			</section>
-			{/* <section className="main-post">
+  return (
+    <MainContainer>
+      <section className="grid">
+        <div className="grid__heading">
+          <h1 className="grid__title" onClick={console.log(postList)}>
+            Filmowy blog
+          </h1>
+          <p className="grid__subtitle">
+            Teksty najlepszych krytyków codziennie
+          </p>
+        </div>
+        <aside className="grid__sidebar">
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li>8</li>
+          </ul>
+        </aside>
+        {postList == null
+          ? ""
+          : postList.map((post, index) => {
+              if (index == 0) {
+                return (
+                  <PostCard
+                    key={index}
+                    post={post}
+                    style={{ gridColumn: "span 2", gridRow: "2 / span 2" }}
+                    imgsize={400}
+                  />
+                );
+              } else {
+                return <PostCard key={index} post={post} />;
+              }
+            })}
+      </section>
+      {/* <section className="main-post">
 				<h1 className="main-post__header">Tytuł całego bloga</h1>
 				<p className="main-post__subheader">Podtytuł mojego bloga</p>
 				<div className="main-post__view">
@@ -100,8 +102,8 @@ const HomePage = () => {
 					</div>
 				</div>
 			</section> */}
-		</MainContainer>
-	);
+    </MainContainer>
+  );
 };
 
 export default HomePage;
