5c6fd3e6ded8a78996e14a9d3fb0ff1450c217c2
Poncjusz Miglanc <poncjuszmiglanc@gmail.com>
2024-01-31 23:13:22 +0100

    38th; CreatePost: + axios & async/await syntax in createPost func,+ createTags func; PostCard: + interpolation that displays list of tags

--- src/Components/PostCard.jsx
+++ src/Components/PostCard.jsx
@@ -1,52 +1,50 @@
-import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
-import PostInfo from "./PostInfo";
-import "../assets/scss/components/post-card.scss";
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import PostInfo from './PostInfo';
+import '../assets/scss/components/post-card.scss';
 
 const PostCard = ({ post, style, imgsize }) => {
-  return (
-    <article className="post-card" style={style}>
-      <Link to={`/posts/${post._id}`}>
-        <div className="post-card__wrapper">
-          <div className="post-card__main-content">
-            <img
-              className="post-card__image"
-              src={`http://localhost:8080/images/${post.image}`}
-              alt="screenshot"
-              style={{ height: `${imgsize}px` }}
-            />
-            <div className="post-card__text">
-              <PostInfo item={post} style={"#6c757d"} />
-              <h2 className="post-card__title">{!post ? "" : post.title}</h2>
-              <p className="post-card__subtitle">
-                {!post ? "" : `${post.lead.substring(1, 200)}...`}
-              </p>
-            </div>
-          </div>
-          <div className="post-card__tags">
-            <span className="post-card__tag">
-              <a href="">Film</a>
-            </span>
-            <span className="post-card__tag">
-              <a href="">Złota palma</a>
-            </span>
-            <span className="post-card__tag">
-              <a href="">Dramat</a>
-            </span>
-            <span className="post-card__tag">
-              <a href="">Antonioni</a>
-            </span>
-          </div>
-        </div>
-      </Link>
-    </article>
-  );
+	return (
+		<article className="post-card" style={style}>
+			<Link to={`/posts/${post._id}`}>
+				<div className="post-card__wrapper">
+					<div className="post-card__main-content">
+						<img
+							className="post-card__image"
+							src={`http://localhost:8080/images/${post.image}`}
+							alt="screenshot"
+							style={{ height: `${imgsize}px` }}
+						/>
+						<div className="post-card__text">
+							<PostInfo item={post} style={'#6c757d'} />
+							<h2 className="post-card__title">{!post ? '' : post.title}</h2>
+							<p className="post-card__subtitle">
+								{!post ? '' : `${post.lead.substring(1, 200)}...`}
+							</p>
+						</div>
+					</div>
+					{/* możliwe, że z tego zrobię komponent */}
+					<div className="post-card__tags">
+						{!post
+							? ''
+							: post.tags.map((tag) => {
+									return (
+										<span key={post._id} className="post-card__tag">
+											<a href="">{tag}</a>
+										</span>
+									);
+							  })}
+					</div>
+				</div>
+			</Link>
+		</article>
+	);
 };
 
 PostCard.propTypes = {
-  post: PropTypes.object,
-  style: PropTypes.object,
-  imgsize: PropTypes.number,
+	post: PropTypes.object,
+	style: PropTypes.object,
+	imgsize: PropTypes.number,
 };
 
 export default PostCard;
--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,174 +1,190 @@
-import { useState } from "react";
-import ReactQuill from "react-quill";
-import MainContainer from "../Components/MainContainer";
-import "react-quill/dist/quill.snow.css";
-import "../assets/scss/pages/create-post.scss";
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import MainContainer from '../Components/MainContainer';
+import 'react-quill/dist/quill.snow.css';
+import '../assets/scss/pages/create-post.scss';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const modules = {
-  toolbar: [
-    [{ header: "1" }, { header: "2" }],
-    ["bold", "italic", "underline", "strike", "blockquote"],
-    [{ list: "ordered" }, { list: "bullet" }],
-    ["link", "image", "video"],
-    ["clean"],
-  ],
+	toolbar: [
+		[{ header: '1' }, { header: '2' }],
+		['bold', 'italic', 'underline', 'strike', 'blockquote'],
+		[{ list: 'ordered' }, { list: 'bullet' }],
+		['link', 'image', 'video'],
+		['clean'],
+	],
 };
 
 const formats = [
-  "header",
-  "font",
-  "size",
-  "bold",
-  "italic",
-  "underline",
-  "strike",
-  "blockquote",
-  "list",
-  "bullet",
-  "link",
-  "image",
-  "video",
+	'header',
+	'font',
+	'size',
+	'bold',
+	'italic',
+	'underline',
+	'strike',
+	'blockquote',
+	'list',
+	'bullet',
+	'link',
+	'image',
+	'video',
 ];
 
 const CreatePost = () => {
-  //chcę, żeby to było szersze, ale to potem
+	//chcę, żeby to było szersze, ale to potem
 
-  const [kategoria, setKategoria] = useState("recenzja");
-  const [tytuł, setTytuł] = useState("");
-  const [lead, setLead] = useState("");
-  const [autor, setAutor] = useState("");
-  const [image, setImage] = useState("");
-  const [treść, setTreść] = useState("");
-  // const [contentForm, setContentForm] = useState("");
+	const [kategoria, setKategoria] = useState('recenzja');
+	const [tytuł, setTytuł] = useState('');
+	const [lead, setLead] = useState('');
+	const [image, setImage] = useState('');
+	const [treść, setTreść] = useState('');
+	const [tags, setTags] = useState([]);
 
-  const createPost = (e) => {
-    e.preventDefault();
+	const navigate = useNavigate();
 
-    const formData = new FormData();
-    formData.append("category", kategoria);
-    formData.append("title", tytuł);
-    formData.append("lead", lead);
-    formData.append("author", autor);
-    formData.append("image", image);
-    formData.append("content", treść);
+	const createTags = (e) => {
+		const trimmedTags = e.target.value.replace(/\s{2,}/g, ' ');
+		// const separatedTags = trimmedTags.split(',').map((tag) => tag.trim());
+		setTags(trimmedTags);
+	};
 
-    fetch("http://localhost:8080/post", {
-      method: "POST",
-      body: formData,
-      credentials: "include",
-    })
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+	const createPost = async (e) => {
+		e.preventDefault();
 
-    setKategoria("recenzja");
-    setTytuł("");
-    setLead("");
-    setAutor("");
-    setImage("");
-    setTreść("");
+		const formData = new FormData();
+		formData.append('category', kategoria);
+		formData.append('title', tytuł);
+		formData.append('lead', lead);
+		formData.append('image', image);
+		formData.append('tags', tags);
+		formData.append('content', treść);
 
-    console.log("wysłano");
-  };
+		// formData.forEach((val, key) => {
+		// 	console.log(key, val);
+		// });
 
-  return (
-    <MainContainer>
-      <div className="create">
-        <h1 className="create__title">Create Post</h1>
-        <form onSubmit={createPost}>
-          <div className="category">
-            <label className="category__label" htmlFor="kategorie">
-              Kategoria
-            </label>
-            <select
-              className="category__select"
-              name="kategoria"
-              id="kategoria"
-              onChange={(e) => setKategoria(e.target.value)}
-            >
-              <option className="category__option" value="recenzja">
-                Recenzja
-              </option>
-              <option className="category__option" value="artykuł">
-                Artykuł
-              </option>
-              <option className="category__option" value="esej">
-                Esej
-              </option>
-            </select>
-          </div>
-          <div className="input">
-            <label className="input__label" htmlFor="tytuł">
-              Tytuł
-            </label>
-            <input
-              className="input__field"
-              id="tytuł"
-              type="text"
-              placeholder="tytuł tekstu"
-              onChange={(e) => setTytuł(e.target.value)}
-              value={tytuł}
-            />
-          </div>
-          <div className="lead">
-            <label className="lead__label" htmlFor="lead">
-              Lead
-            </label>
-            <textarea
-              className="lead__textfield"
-              name=""
-              id="lead"
-              cols="30"
-              rows="3"
-              value={lead}
-              onChange={(e) => setLead(e.target.value)}
-            ></textarea>
-          </div>
-          <div className="input">
-            <label className="input__label" htmlFor="autor">
-              Autor
-            </label>
-            <input
-              className="input__field"
-              id="autor"
-              type="text"
-              placeholder="Autor wpisu"
-              onChange={(e) => setAutor(e.target.value)}
-              value={autor}
-            />
-          </div>
-          <div className="photo">
-            <label className="photo__label" htmlFor="photo">
-              Dodaj grafikę
-            </label>
-            <input
-              className="photo__input"
-              type="file"
-              onChange={(e) => setImage(e.target.files[0])}
-              id="photo"
-            />
-          </div>
-          <div className="quill-container">
-            <ReactQuill
-              className="quill-container__my-editor"
-              value={treść}
-              onChange={setTreść}
-              modules={modules}
-              formats={formats}
-            />
-          </div>
+		try {
+			const response = await axios.post(
+				'http://localhost:8080/post',
+				formData,
+				{ withCredentials: true }
+			);
 
-          <button className="create__btn">Opublikuj</button>
-        </form>
-      </div>
-    </MainContainer>
-  );
+			if (response.status >= 200 && response.status < 300) {
+				console.log('utworzono posta', response.data);
+				navigate('/');
+			} else {
+				console.log('błąd podczas tworzenia posta');
+			}
+
+			setKategoria('recenzja');
+			setTytuł('');
+			setLead('');
+			setImage('');
+			setTreść('');
+			setTags([]);
+		} catch (err) {
+			console.log('wystąpił błąd podczas rządania: ', err);
+		}
+	};
+
+	return (
+		<MainContainer>
+			<div className="create">
+				<h1 className="create__title">Create Post</h1>
+				<form onSubmit={createPost}>
+					<div className="category">
+						<label className="category__label" htmlFor="kategorie">
+							Kategoria
+						</label>
+						<select
+							className="category__select"
+							name="kategoria"
+							id="kategoria"
+							onChange={(e) => setKategoria(e.target.value)}
+						>
+							<option className="category__option" value="recenzja">
+								Recenzja
+							</option>
+							<option className="category__option" value="artykuł">
+								Artykuł
+							</option>
+							<option className="category__option" value="esej">
+								Esej
+							</option>
+						</select>
+					</div>
+
+					<div className="input">
+						<label className="input__label" htmlFor="tytuł">
+							Tytuł
+						</label>
+						<input
+							className="input__field"
+							id="tytuł"
+							type="text"
+							placeholder="tytuł tekstu"
+							onChange={(e) => setTytuł(e.target.value)}
+							value={tytuł}
+						/>
+					</div>
+
+					<div className="lead">
+						<label className="lead__label" htmlFor="lead">
+							Lead
+						</label>
+						<textarea
+							className="lead__textfield"
+							name=""
+							id="lead"
+							cols="30"
+							rows="3"
+							value={lead}
+							onChange={(e) => setLead(e.target.value)}
+						></textarea>
+					</div>
+
+					<div className="photo">
+						<label className="photo__label" htmlFor="photo">
+							Dodaj grafikę
+						</label>
+						<input
+							className="photo__input"
+							type="file"
+							onChange={(e) => setImage(e.target.files[0])}
+							id="photo"
+						/>
+					</div>
+
+					<div className="input">
+						<label className="input__label" htmlFor="tags">
+							Dodaj tagi
+						</label>
+						<input
+							className="input__field"
+							type="text"
+							id="tags"
+							onChange={createTags}
+						/>
+					</div>
+
+					<div className="quill-container">
+						<ReactQuill
+							className="quill-container__my-editor"
+							value={treść}
+							onChange={setTreść}
+							modules={modules}
+							formats={formats}
+						/>
+					</div>
+
+					<button className="create__btn">Opublikuj</button>
+				</form>
+			</div>
+		</MainContainer>
+	);
 };
 
 export default CreatePost;
